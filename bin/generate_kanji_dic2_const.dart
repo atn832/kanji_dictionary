c05e8844726848b1599847d185fa638b79cd501a
Anh Tuan <atn832@gmail.com>
2022-12-07 18:39:00 +0900

    make the generation of kanji_dic2 pass format check

--- bin/generate_kanji_dic2_const.dart
+++ bin/generate_kanji_dic2_const.dart
@@ -4,5 +4,5 @@ import 'dart:io';
 main() {
   final xml = File('./assets/kanjidic2.xml').readAsStringSync();
   File('./lib/src/kanjidic2xml.dart')
-      .writeAsStringSync("const kanjiDic2Xml = '''$xml''';");
+      .writeAsStringSync("const kanjiDic2Xml = '''$xml''';\n");
 }
