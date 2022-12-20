cea3e6986668c73571aa0abf86872a55084306d1
Anh Tuan <atn832@gmail.com>
2022-12-20 12:49:25 +0900

    make the script download the latest version of the dictionary

--- bin/generate_kanji_dic2_const.dart
+++ bin/generate_kanji_dic2_const.dart
@@ -1,8 +1,18 @@
+import 'dart:convert';
 import 'dart:io';
 
-/// Generates kanjidic2xml.dart, which kanji_dictionary uses for initialization.
-main() {
-  final xml = File('./assets/kanjidic2.xml').readAsStringSync();
+// Since this script is used only when developing, I added http as a dev
+// dependency and we can ignore the warning.
+// ignore: depend_on_referenced_packages
+import 'package:http/http.dart';
+
+/// Generates kanjidic2xml.dart from the official KANJIDIC repository.
+/// kanji_dictionary uses that file for initialization.
+main() async {
+  final xml = utf8.decode(gzip.decode(
+      (await get(Uri.parse('http://www.edrdg.org/kanjidic/kanjidic2.xml.gz')))
+          .bodyBytes));
+  File('./assets/kanjidic2.xml').writeAsStringSync(xml);
   File('./lib/src/kanjidic2xml.dart')
       .writeAsStringSync("const kanjiDic2Xml = '''$xml''';\n");
 }
